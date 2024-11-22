35efaff2cdcfd2f8d48afbec18bf3e560c34210e
Chabry <chabry01@gmail.com>
2024-11-22 15:37:49 +0100

    Exercice résolu

--- 03-08-data-types-typeof.js
+++ 03-08-data-types-typeof.js
@@ -3,16 +3,23 @@
 // Function 1
 function getType(value) {
   // Your code here
+  return typeof(value)
 }
 
 // Function 2
 function checkTypes(...values) {
   // Your code here
+  return values.map(value => typeof value);
 }
 
 // Function 3
 function compareTypes(value1, value2) {
   // Your code here
+  if(typeof(value1) == typeof(value2)){
+    return true
+  }else{
+    return false
+  }
 }
 
 module.exports = {
